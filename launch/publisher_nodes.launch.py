d9514b1e31ea6796d2036c0e4e4ee7211d2b6e45
suzakutakumi <suzakutakumi3@gmail.com>
2024-09-30 11:37:29 +0900

    DTクライアントにパラメータを追加

--- launch/publisher_nodes.launch.py
+++ launch/publisher_nodes.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     # launchの構成を示すLaunchDescription型の変数の定義
     ld = LaunchDescription()
@@ -10,17 +11,16 @@ def generate_launch_description():
         package='pressure',
         executable='pressure_subscriber',
         name='pressure',
-        #namespace='pressure',                 # namespace_app2というnamespaceを追加
-        remappings=[('image_topic', 'pressure_image')]    # chatterトピックをchatter_app2トピックにremap
+        remappings=[('image_topic', 'pressure_image')]
     )
 
     # publisher node "QR"
     pub_node2 = Node(
         package='scan_qr',
         executable='listener',
-        name='listener',
-        #namespace='pressure',                 # namespace_app2というnamespaceを追加
-        remappings=[('image_raw', 'qr_image'),('qr_image','qr_result_image'),("qr_value","qr_result_value")]    # chatterトピックをchatter_app2トピックにremap
+        name='qr',
+        remappings=[('image_raw', 'qr_image'), ('qr_image', 'qr_result_image'),
+                    ("qr_value", "qr_result_value")]
     )
 
     # publisher node "Rust"
@@ -28,8 +28,7 @@ def generate_launch_description():
         package='rust',
         executable='rust_subscriber',
         name='rust',
-        #namespace='rust',                 # namespace_app1というnamespaceを追加
-        remappings=[('image_topic', 'rust_image')]    # chatterトピックをchatter_app1トピックにremap
+        remappings=[('image_topic', 'rust_image')]
     )
 
     # publisher node "Crack"
@@ -37,45 +36,31 @@ def generate_launch_description():
         package='crack',
         executable='crack_subscriber',
         name='crack',
-        #namespace='crack',                 # namespace_app1というnamespaceを追加
-        remappings=[('image_raw', 'crack_image')]    # chatterトピックをchatter_app1トピックにremap
+        remappings=[('image_raw', 'crack_image')]
     )
 
     pub_node5 = Node(
         package='tree_publisher',
         executable='tree_publisher',
-        # name='tree',
-        #namespace='crack',                 # namespace_app1というnamespaceを追加
-        remappings=[('input_image_topic', '/arm_camera/image_raw/decompressed')]    # chatterトピックをchatter_app1トピックにremap
+        name='tree',
+        remappings=[
+            ('input_image_topic', '/arm_camera/image_raw/decompressed')]
     )
 
     pub_node6 = Node(
         package='digital_twin_client',
         executable='digital_twin_client',
-        # name='tree',
-        #namespace='crack',                 # namespace_app1というnamespaceを追加
-        remappings=[('send_image', 'send_topic')]    # chatterトピックをchatter_app1トピックにremap
+        name='dt_client',
+        remappings=[('send_image', 'send_topic')],
+        parameters=[{"host": "", "robot_id": ""}]  # サーバの値を設定する
     )
 
-    # pub_node6 = Node(
+    # pub_node7 = Node(
     #     package='temp_publisher',
     #     executable='temp_publisher',
-    #     name='temp'
-    #     #namespace='crack',                 # namespace_app1というnamespaceを追加
-    #     # remappings=[('image_raw', 'crack_image')]    # chatterトピックをchatter_app1トピックにremap
+    #     name='temp',
+    #     remappings=[('image_raw', 'crack_image')]
     # )
-
-
-
-    # publisher nodeを、"talker_renamed1"という名前で定義
-    # pub_node6 = Node(
-    #     package='send',
-    #     executable='image_subscriber',
-    #     name='crack_subscriber',
-    #     #namespace='crack',                 # namespace_app1というnamespaceを追加
-    #     remappings=[('image_raw', 'crack_image')]    # chatterトピックをchatter_app1トピックにremap
-    # )
-
 
     # LaunchDescriptionに、起動したいノードを追加する
     ld.add_action(pub_node1)
@@ -84,6 +69,7 @@ def generate_launch_description():
     ld.add_action(pub_node4)
     ld.add_action(pub_node5)
     ld.add_action(pub_node6)
+    # ld.add_action(pub_node7)
 
     # launch構成を返すようにする
     return ld
